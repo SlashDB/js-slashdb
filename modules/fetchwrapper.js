87ea335c7e2de036bb908bfc0d456e508d1dc369
jh-projects <79557972+jh-projects@users.noreply.github.com>
2022-09-01 16:02:05 -0400

    refactor
    - refactored fetchWrapper to use pure awaits, stripped out .then() calls

--- modules/fetchwrapper.js
+++ modules/fetchwrapper.js
@@ -18,29 +18,43 @@
     onlyRes
   ) {
 
+    if (body !== undefined) {
+      if (headers['Content-Type'] === 'application/json') {
+          body = JSON.stringify(body);
+      }
+    }
+    else {
+      body = null;
+    }
     const requestOptions = {
       method: `${httpMethod}`,
       credentials: 'include',
       headers: {
-        'Content-Type': 'application/json',
         ...headers,
       },
-      body: body !== undefined ? JSON.stringify(body) : null,
+      body: body
     };
   
     const returnObj = { data: null, res: null };
 
     try {
-      await fetch(url, requestOptions)
-        .then((response) => {
-          returnObj.res = response;
-          returnObj.data = onlyRes ? null : handleResponse(response);
-        })
-        .catch((error) => {
-          console.log(error.response);
-        });
-    } catch (error) {
-      console.log(error);
+      let response = await fetch(url, requestOptions);
+      //console.log(await response.text())
+      if (response.ok === true) {
+        returnObj.res = response;
+        returnObj.data = onlyRes ? null : handleResponse(response);
+      }
+      else {
+        let errMsg = response.status;
+        let headers = response.headers;
+        if (headers.has('Warning')) {
+          errMsg = `HTTP ${errMsg}: ${headers.get('Warning')}`;
+        }
+        throw Error(errMsg)
+      }
+    } catch (e) {
+      console.error(e);
+      throw Error(e);      
     }
     if (onlyRes) {
       return returnObj.res;
@@ -49,35 +63,86 @@
     }
   }
 
+
   /** Helper function to handle requests response.
  *
  * @param {*} response
  * @returns {Object} data
  */
 async function handleResponse(response) {
-    return await response.text().then((text) => {
-      let data;
-      try {
-          data = text && JSON.parse(text);
-      }
-      // for urls that aren't JSON data return the raw data
-      catch(e) {
-          if (e instanceof SyntaxError) {
-              data = text;
-          }
-          else {
-              console.error("Error encountered in handleResponse");
-              return Promise.reject("Error encountered in handleResponse")
-          }
-      }
-  
-      if (!response.ok) {
-        const error = (data && data.message) || response.statusText;
-        return Promise.reject(error);
-      }
-  
-      return data;
-    });
+  let data, text;
+  try {
+    data = await response.text()
+    text = data;
+    data = data && JSON.parse(data);
   }
+  // for urls that aren't JSON data return the raw data
+  catch(e) {
+      if (e instanceof SyntaxError) {
+          data = text;
+      }
+      else {
+          console.error("Error encountered in handleResponse");
+          return Promise.reject("Error encountered in handleResponse")
+      }
+  }
+
+  if (!response.ok) {
+    const error = (data && data.message) || response.statusText;
+    return Promise.reject(error);
+  }
+
+  return data;
+}
+
+
+  // fetchWrapper old
+  //   try {
+  //     let resposne await fetch(url, requestOptions)
+  //       .then((response) => {
+  //         returnObj.res = response;
+  //         returnObj.data = onlyRes ? null : handleResponse(response);
+  //       })
+  //       .catch((error) => {
+  //         throw Error(error.response);
+  //         console.log(error.response);
+  //       });
+  //   } catch (error) {
+  //     throw Error(error.response);      
+  //     console.log(error);
+  //   }
+  //   if (onlyRes) {
+  //     return returnObj.res;
+  //   } else {
+  //     return [returnObj.data, returnObj.res];
+  //   }
+  // }
+
+
+// async function handleResponse(response) {
+//     return await response.text().then((text) => {
+//       let data;
+//       try {
+//           data = text && JSON.parse(text);
+//       }
+//       // for urls that aren't JSON data return the raw data
+//       catch(e) {
+//           if (e instanceof SyntaxError) {
+//               data = text;
+//           }
+//           else {
+//               console.error("Error encountered in handleResponse");
+//               return Promise.reject("Error encountered in handleResponse")
+//           }
+//       }
+  
+//       if (!response.ok) {
+//         const error = (data && data.message) || response.statusText;
+//         return Promise.reject(error);
+//       }
+  
+//       return data;
+//     });
+//   }
 
   export { fetchWrapper }
