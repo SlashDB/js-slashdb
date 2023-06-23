3493cb498e4a34e10d233c2da4d16f5b7c9ec21d
jhslashdb <109535123+jhslashdb@users.noreply.github.com>
2023-06-23 14:37:56 -0400

    package file and jest config

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,9 @@ module.exports = {
   globals: {
     LIVE_TESTS_ENABLED: true,
 	MOCK_TESTS_ENABLED: true,
-	LIVE_SDB_HOST: 'http://192.168.1.101:8000',
-	LIVE_SDB_API_KEY: 'gez0bk949rhwpcjdxqqsn8w9g4iec709',
+	LIVE_SDB_HOST: 'http://127.0.0.1:8080',
+	LIVE_SDB_API_KEY: '<API_KEY_HERE>',
+	SDB_TEST_DB_NAME: 'Chinook',
 	MOCK_HOST: 'http://localhost'
   }
 };
