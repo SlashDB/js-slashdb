1958035cd0dfba4898a7f2aa49aff5c9118c003b
Nelson Carrasquel <carrasquel@outlook.com>
2023-11-17 09:29:00 -0400

    cryptographic secured functions added

--- src/utils.js
+++ src/utils.js
@@ -31,6 +31,8 @@ function generateRandomString(length) {
     return text;
 }
 
+// Generate Code Verifier
+
 function dec2hex(dec) {
     return ("0" + dec.toString(16)).substr(-2);
 }
@@ -41,6 +43,35 @@ function generateCodeVerifier(size = 128) {
 
     return Array.from(array, dec2hex).join("");
 }
+
+// Generate code challenge from code verifier
+
+function sha256(plain) {
+    // returns promise ArrayBuffer
+    const encoder = new TextEncoder();
+    const data = encoder.encode(plain);
+    return window.crypto.subtle.digest("SHA-256", data);
+}
+
+function base64urlencode(a) {
+    var str = "";
+    var bytes = new Uint8Array(a);
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        str += String.fromCharCode(bytes[i]);
+    }
+    return btoa(str)
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=+$/, "");
+}
+
+async function generateCodeChallengeFromVerifier(v) {
+    var hashed = await sha256(v);
+    var base64encoded = base64urlencode(hashed);
+    return base64encoded;
+}
+
 
 function generateCodeChallenge(code_verifier) {
     return base64URL(SHA256(code_verifier));
