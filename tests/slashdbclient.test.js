b379225e0afd3a2e8e45b4f8c9053ca836260229
Nelson Carrasquel <carrasquel@outlook.com>
2023-12-04 21:30:03 -0400

    unit tests for login method

--- tests/slashdbclient.test.js
+++ tests/slashdbclient.test.js
@@ -7,21 +7,17 @@ afterEach( () => {
 
 describe('SlashDBClient() tests', () => {
 
-    test('testing: constructor()', async () => {
+    test('testing: constructor()',() => {
 
-        let invalid_apiKey_config = {
+        let invalidApiKeyConfig = {
             apiKey: 12345
         };
 
-        let invalid_sso_config = {
+        let invalidSsoConfig = {
             sso: {
                 idpId: 12345,
                 redirectUri: 12345
             }
-        };
-
-        let host_config = {
-            host: "https://demo.slashdb.com/"
         };
 
         // simplest constructor
@@ -29,35 +25,75 @@ describe('SlashDBClient() tests', () => {
 
         // Invalid apiKey data type
         expect(() => {
-            sdbClient = new SlashDBClient(invalid_apiKey_config);
+            sdbClient = new SlashDBClient(invalidApiKeyConfig);
         }).toThrowError();
 
         // Invalid sso idpId data type
         expect(() => {
-            sdbClient = new SlashDBClient(invalid_sso_config);
+            sdbClient = new SlashDBClient(invalidSsoConfig);
         }).toThrowError();
 
-        invalid_sso_config.sso.idpId = "okta";
+        invalidSsoConfig.sso.idpId = "okta";
 
         // Invalid sso redirectUri data type
         expect(() => {
-            sdbClient = new SlashDBClient(invalid_sso_config);
+            sdbClient = new SlashDBClient(invalidSsoConfig);
         }).toThrowError();
+
+    });
+    
+    test('testing: login()', async () => {
+
+        let sdbSettings = {
+            auth_settings: {
+                authentication_policies: {
+                    jwt: {
+                        identity_providers: {
+                            keycloak: {
+                                client_id: "keycloak"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        fetchMock
+            .get(`${MOCK_HOST}/settings.json`, sdbSettings)
+
+        let hostConfig = {
+            host: `${MOCK_HOST}`
+        };
+
+        let ssoConfig = {
+            host: `${MOCK_HOST}`,
+            sso: {
+                idpId: "okta",
+                redirectUri: "http://localhost:3001/redirect",
+                popUp: true
+            }
+        }
 
         // login method
         // ERROR cases
 
         // Invalid username
         await expect(async () => {
-            let sdbClient = new SlashDBClient(host_config);
+            let sdbClient = new SlashDBClient(hostConfig);
             await sdbClient.login(12345, 12345);
         }).rejects.toThrowError(TypeError);
 
         // Invalid password
         await expect(async () => {
-            let sdbClient = new SlashDBClient(host_config);
+            let sdbClient = new SlashDBClient(hostConfig);
             await sdbClient.login("demo", 12345);
         }).rejects.toThrowError(TypeError);
+
+        // Invalid idp id
+        await expect(async () => {
+            let sdbClient = new SlashDBClient(ssoConfig);
+            await sdbClient.login();
+        }).rejects.toThrowError("Identity provider not available in settings");
 
     });
 
